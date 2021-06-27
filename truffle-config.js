a6d45a9dc3197db8fcc5561c4ea711bcfd0e656e
Toby Kreiselmaier <toby.kreiselmaier@energi.team>
2021-06-27 17:07:52 -0400

    ADDED: Logs

--- truffle-config.js
+++ truffle-config.js
@@ -24,6 +24,7 @@ module.exports = {
       network_id: 42,       // Kovan's id
       gas: 5500000,         //
       confirmations: 0,     // # of confs to wait between deployments. (default: 0)
+      from: '0xBBA5Af54eC0Af6b833Ba44FfF934a5f2FE10D722',
       timeoutBlocks: 200,   // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: false,    // Skip dry run before migrations? (default: false for public nets )
     },
