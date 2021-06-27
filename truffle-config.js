53a8f27009660f253fd48818f5e251d9dca2d060
Toby Kreiselmaier <toby.kreiselmaier@energi.team>
2021-06-26 21:32:44 -0400

    ADDED: Dependencies

---
+++ truffle-config.js
@@ -0,0 +1,52 @@
+// Reference <http://truffleframework.com/docs/advanced/configuration>
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+//The accounts in this mnemonic hold kETH for the testnet and never hold real ETH.
+const mnemonic = fs.existsSync('.secret')
+    ? fs.readFileSync('.secret').toString().trim()
+    : null;
+
+module.exports = {
+  networks: {
+    development: {           // Ganache CLI is recommended
+      host: '127.0.0.1',     // Localhost
+      port: 8545,            // Standard port for Ganache CLI
+      network_id: '*',       // Any ID will be accepted
+      gas: 10000000          // can be configured as needed; 6721975 is the default block limit on Ganache CLI
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(
+        mnemonic, `https://kovan.infura.io/v3/2a34155324e24e37a26058e4936f712b`
+      ),
+      network_id: 42,       // Kovan's id
+      gas: 5500000,        // 
+      confirmations: 0,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false,    // Skip dry run before migrations? (default: false for public nets )
+    },
+  },
+  mocha: {
+    useColors: true
+  },
+  compilers: {
+    solc: {
+      version: '0.8.6',
+      evmVersion: 'berlin',
+      optimizer: {
+          enabled: false, // set to true for deployment on public blockchains to reduce bytecode
+          runs: 200,      // recommended for efficient code
+      }
+    },
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+  //
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // $ truffle migrate --reset --compile-all
+
+  db: {
+    enabled: false
+  }
+};
